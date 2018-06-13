b84e9224e517d95db17c2034566f015adae69a8e
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2018-06-14 01:16:27 +0300

    cid

--- src/nolipservice/google_analytics.clj
+++ src/nolipservice/google_analytics.clj
@@ -2,14 +2,17 @@
   (:require [clj-http.client :as client]))
 
 
-(defn report [el v]
-  (client/post "https://www.google-analytics.com/collect" {:form-params {:v "1"
-                                                                         :tid "UA-42102404-4"
-                                                                         :t "event"
-                                                                         :ec "meyvn"
-                                                                         :ea "build"
-                                                                         :el el
-                                                                         :ev v
-                                                                         :uid "org.danielsz"
-                                                                         :ni "1"}}))
+(defn report [cid el v & {:keys [debug] :or {debug false}}]
+  (let [endpoint (if debug
+                   "https://www.google-analytics.com/debug/collect"
+                   "https://www.google-analytics.com/collect")]
+    (client/post endpoint {:form-params {:v "1"
+                                         :tid "UA-42102404-4"
+                                         :t "event"
+                                         :ec "meyvn"
+                                         :ea "build"
+                                         :cid cid
+                                         :el el
+                                         :ev v
+                                         :ni "1"}})))
 
